5d2b08b14a416c5a7abf3c79e25c60bae1ee4a9e
Mathias G <ghbm@aarhus.dk>
2024-06-04 13:13:16 +0200

    Process implemented

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,8 +1,31 @@
 """This module contains the main process of the robot."""
 
+import secrets
+
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
+from itk_dev_shared_components.sap import sap_login
 
 
 def process(orchestrator_connection: OrchestratorConnection) -> None:
     """Do the primary process of the robot."""
     orchestrator_connection.log_trace("Running process.")
+
+    credential_names = orchestrator_connection.process_arguments.split(",")
+
+    for name in credential_names:
+        orchestrator_connection.log_info(f"Changing password for {name}.")
+
+        new_password = create_password()
+        cred = orchestrator_connection.get_credential(name)
+        sap_login.change_password(username=cred.username, old_password=cred.password, new_password=new_password)
+
+    orchestrator_connection.log_info(f"Changed {len(credential_names)} passwords.")
+
+
+def create_password() -> str:
+    """Generate a 16 length password containing [A-Z][a-z][_-].
+
+    Returns:
+        A random 16 length string.
+    """
+    return secrets.token_urlsafe(16)[:16]
